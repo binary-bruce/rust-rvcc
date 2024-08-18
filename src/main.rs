2751a7fd55371329ff41bb3833322b2c4ce03602
bruce.li <127000969+binary-bruce@users.noreply.github.com>
2024-08-18 16:57:44 +0800

    feat: support + and - (#2)

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,55 @@ fn main() {
         panic!("invalid number of arguments");
     }
 
-    if let Some(value) = args.last() {
-        println!("  .globl main");
-        println!("main:");
-        println!("  li a0, {}", value.parse::<i32>().unwrap());
-        println!("  ret");
+    let input = args.last().unwrap();
+    let (mut ops, mut numbers) = tokenize(input);
+
+    println!("  .globl main");
+    println!("main:");
+
+    println!("  li a0, {}", numbers.pop().unwrap());
+
+    while let Some(number) = numbers.pop() {
+        let op = ops.pop().unwrap();
+        match op {
+            '+' => {
+                println!("  addi a0, a0, {}", number);
+            }
+
+            '-' => {
+                println!("  addi a0, a0, -{}", number);
+            }
+
+            _ => panic!("cannot reach here"),
+        }
     }
+
+    println!("  ret");
+}
+
+fn tokenize(input: &String) -> (Vec<char>, Vec<usize>) {
+    let input_chars = input.chars();
+    let mut ops: Vec<char> = vec![];
+    let mut numbers: Vec<usize> = vec![];
+    let mut current = 0;
+    for (i, c) in input_chars.enumerate() {
+        if c == '+' || c == '-' {
+            ops.push(c);
+
+            let number = to_number(&input[current..i]);
+            numbers.push(number);
+            current = i + 1;
+        }
+    }
+
+    numbers.push(to_number(&input[current..]));
+
+    ops.reverse();
+    numbers.reverse();
+
+    (ops, numbers)
+}
+
+fn to_number(s: &str) -> usize {
+    s.trim().parse::<usize>().unwrap()
 }
